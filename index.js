3499fe01f83450eef782ee34a4b4d1e19b91b2c6
gianniresende <gianniresende@gmail.com>
2020-04-26 23:53:55 -0300

    session user

--- index.js
+++ index.js
@@ -5,11 +5,38 @@ const path = require('path')
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
+const bodyParser = require('body-parser')
+const session = require('express-session')
+
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded())
+app.use(session({
+  secret: 'socketio',
+  cookie: {
+    maxAge: 10*60*1000
+  }
+}))
 
 app.set('views', path.join(__dirname, 'src/views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('home'))
+app.post('/', (req,res) => {
+  req.session.user = {
+    name: req.body.name
+  }
+  res.redirect('/room')
+})
+
+app.get('/room', (req, res) => {
+  if(!req.session.user){
+    res.redirect('/')
+  }else{
+    res.render('room', {
+      name: req.session.user.name
+    })
+  }
+})
+
 mongoose
   .connect('mongodb://localhost/chat-socketio')
   .then(() => {
